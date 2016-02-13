f91048e171bdb491e8f5bce14d8d2aa78bb4b588
Sujesh Kumar U V <sujeshkumaruv2@gmail.com>
2016-02-13 20:16:35 +0530

    convert get todos to sequelize

--- server.js
+++ server.js
@@ -29,7 +29,26 @@ app.get('/',function(req,res){
 });
 
 app.get('/todos',function(req,res){
-	res.json(todos);
+	var query = req.query;
+	var where = {};
+	
+	if(query.hasOwnProperty('completed') && query.completed === "true") {
+		where.completed = true;
+	} else if(query.hasOwnProperty('completed') && query.completed === "false" ) {
+		where.completed = false;
+	}
+
+	if(query.hasOwnProperty('q') && query.q.length > 0) {
+		where.description = {
+			$like: '%'+ query.q + '%'
+		}
+	}
+
+	db.todo.findAll({where: where}).then(function(todos){
+		res.json(todos);
+	},function(e) {
+		res.status(500).send();
+	})
 });
 
 app.get('/todos/:id',function(req,res){
